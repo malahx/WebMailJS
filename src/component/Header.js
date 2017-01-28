b12ae4db30b8e6d4ebbe75e5838391d78ea596c6
Malah <malah@neuf.fr>
2017-01-28 10:56:04 +0100

    merging imports, css class correction

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import FA from 'react-fontawesome';
-import { Button } from 'react-bootstrap';
-import { ButtonGroup } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
-import { FormGroup } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap';
-import { InputGroup } from 'react-bootstrap';
-import { Glyphicon } from 'react-bootstrap';
-import { ButtonToolbar } from 'react-bootstrap';
+import { Button, ButtonGroup, Navbar, FormGroup, FormControl, InputGroup, Glyphicon, ButtonToolbar } from 'react-bootstrap';
 import './Header.css';
 
 class Header extends Component {
@@ -23,18 +16,18 @@ class Header extends Component {
                 <Navbar.Text>
                     <ButtonToolbar>
                         <ButtonGroup>
-                            <Button bsStyle="success" bsSize="small" class="navbar-btn"><FA name='pencil-square-o' /> Ecrire</Button>
-                            <Button bsStyle="info" bsSize="small" class="navbar-btn"><FA name='refresh' /></Button>
-                            <Button bsStyle="primary" bsSize="small" class="navbar-btn"><FA name='cog' /></Button>
+                            <Button bsStyle="success" bsSize="small"><FA name='pencil-square-o' /> Ecrire</Button>
+                            <Button bsStyle="info" bsSize="small"><FA name='refresh' /></Button>
+                            <Button bsStyle="primary" bsSize="small"><FA name='cog' /></Button>
                         </ButtonGroup>
                     </ButtonToolbar>
                 </Navbar.Text>
                 <Navbar.Text>
                     <ButtonToolbar>
                         <ButtonGroup>
-                            <Button bsStyle="default" bsSize="small" class="navbar-btn" disabled><FA name='check' /></Button>
-                            <Button bsStyle="warning" bsSize="small" class="navbar-btn" disabled><FA name='hand-paper-o' /></Button>
-                            <Button bsStyle="danger" bsSize="small" class="navbar-btn" disabled><FA name='trash' /></Button>
+                            <Button bsStyle="default" bsSize="small" disabled><FA name='check' /></Button>
+                            <Button bsStyle="warning" bsSize="small" disabled><FA name='hand-paper-o' /></Button>
+                            <Button bsStyle="danger" bsSize="small" disabled><FA name='trash' /></Button>
                         </ButtonGroup>
                     </ButtonToolbar>
                 </Navbar.Text>
