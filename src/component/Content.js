001e8036d231b0219b57c4dc885af381aafb9dde
Malah <malah@neuf.fr>
2017-01-28 11:12:01 +0100

    added content

---
+++ src/component/Content.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Col, Table } from 'react-bootstrap';
+import './Content.css';
+import FA from 'react-fontawesome';
+
+class Content extends Component {
+  render() {
+    return (
+        <Col lg={10}>
+            <Table condensed hover striped>
+                <thead>
+                    <tr>
+                        <th><FA name='tag' /></th>
+                        <th>De</th>
+                        <th>Sujet</th>
+                        <th>Taille</th>
+                        <th>Envoyé</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="bold">
+                        <td><FA name='envelope' /></td>
+                        <td>truc@machin.fr</td>
+                        <td>Tincidunt, turpis sed séllicitudén facîlisis suscipit vehiculâ éléfantid donec conubié
+                            tempès
+                            rhoncüs class arcû, magnès lilitoxic vulputaté libero venenatis eget lectus !
+                        </td>
+                        <td>8 ko</td>
+                        <td>jeu. 17 janv.</td>
+                    </tr>
+                    <tr className="bold">
+                        <td><FA name='envelope' /></td>
+                        <td>truc@machin.fr</td>
+                        <td>Tincidunt, turpis sed séllicitudén facîlisis suscipit vehiculâ éléfantid donec conubié
+                            tempès
+                            rhoncüs class arcû, magnès lilitoxic vulputaté libero venenatis eget lectus !
+                        </td>
+                        <td>8 ko</td>
+                        <td>jeu. 17 janv.</td>
+                    </tr>
+                    <tr className="bold">
+                        <td><FA name='envelope' /></td>
+                        <td>truc@machin.fr</td>
+                        <td>Tincidunt, turpis sed séllicitudén facîlisis suscipit vehiculâ éléfantid donec conubié
+                            tempès
+                            rhoncüs class arcû, magnès lilitoxic vulputaté libero venenatis eget lectus !
+                        </td>
+                        <td>8 ko</td>
+                        <td>jeu. 17 janv.</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </Col>
+    );
+  }
+}
+
+export default Content;
